76c62c7f19bec2b5148849332cde9221d8fcb48a
Daryl Busson <daryljjbb@yahoo.com>
2025-06-10 13:18:27 -0700

    Updated Html Pages

---
+++ register.js
@@ -0,0 +1,26 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+document.querySelector(".log_button").addEventListener("click", function() {
+    window.location.href = "login.html";
+});
+document.querySelector(".reg_button").addEventListener("click", function() {
+    window.location.href = "register.html";
+});
+
+document.addEventListener("scroll", function() {
+    document.querySelectorAll(".scroll-fade").forEach((element) => {
+        let position = element.getBoundingClientRect().top;
+        let windowHeight = window.innerHeight;
+        if (position < windowHeight - 100) {
+            element.classList.add("visible");
+        }
+    });
+});
